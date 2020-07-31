c0ed714e1f31d6d4bd3ba8aeba880d433eb1bbb3
cdup <cdup@MSI>
2020-07-31 12:04:20 +1200

    Add project files.

---
+++ Staircase/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Staircase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBxNumberStaircase = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBxNumberStaircase
+            // 
+            this.txtBxNumberStaircase.Location = new System.Drawing.Point(164, 42);
+            this.txtBxNumberStaircase.Name = "txtBxNumberStaircase";
+            this.txtBxNumberStaircase.Size = new System.Drawing.Size(354, 22);
+            this.txtBxNumberStaircase.TabIndex = 0;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(254, 304);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(174, 54);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtBxNumberStaircase);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBxNumberStaircase;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ Staircase/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Staircase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+           // int n = Convert.ToInt32(Console.ReadLine());
+
+            staircase(Convert.ToInt32(txtBxNumberStaircase.Text));
+        }
+
+        // Complete the staircase function below.
+        static void staircase(int n)
+        {
+            string hash = new string('#', n);
+            string space = new string(' ', n + 1);
+            string holderHash = "";
+            string holderSpace = "";
+            space = space.Remove(0, 1);
+            for (int i = 0; i < n; i++)
+            {
+                space = space.Remove(0, 1);
+                holderSpace = holderSpace + space;
+                holderHash = holderHash + hash.Remove(0, hash.Length - 1);
+                Console.WriteLine(space + holderHash);
+            }
+        }
+    }
+}
